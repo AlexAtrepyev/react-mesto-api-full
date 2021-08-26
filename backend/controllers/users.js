6cea4b3112778229444d155d4e0dc039debb054f
Alex <atrepyev27@yandex.ru>
2021-08-26 19:22:55 +0300

    fix: -

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -75,7 +75,7 @@ module.exports.login = (req, res, next) => {
   User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, 'some-secret-key', { expiresIn: '7d' });
-      res.cookie('jwt', token, { maxAge: 3600000 * 24 * 7, httpOnly: false }).end();
+      res.cookie('jwt', token, { maxAge: 3600000 * 24 * 7, httpOnly: true }).end();
     })
     .catch(next);
 };
