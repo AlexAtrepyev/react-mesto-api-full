91d66489bcb0b41511017d21e90b091020032517
Alex <atrepyev27@yandex.ru>
2021-08-28 19:51:43 +0300

    fix

--- backend/app.js
+++ backend/app.js
@@ -38,7 +38,7 @@ app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().required().pattern(/^https?:\/\/(w{3}.)?[^а-яё\s]*/),
+    avatar: Joi.string().pattern(/^https?:\/\/(w{3}.)?[^а-яё\s]*/),
     email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
