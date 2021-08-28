00953e02d62be4d5a365b76575772033e434bcda
Alex <atrepyev27@yandex.ru>
2021-08-28 11:22:35 +0300

    fix

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -7,7 +7,8 @@ module.exports = (req, res, next) => {
 
   let payload;
   try {
-    payload = jwt.verify(token, 'some-secret-key');
+    const { NODE_ENV, JWT_SECRET } = process.env;
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     throw new AuthError(401, 'Необходима авторизация');
   }
